9c465aa139f4179596f0251063da4d78a76c21dd
FMB_Player <franbalella@hotmail.com.ar>
2025-07-01 21:01:14 -0300

    APP's skeleton

---
+++ main.py
@@ -0,0 +1,12 @@
+from classes import *   # Import all classes
+
+# Resources initialization
+pass
+
+# Main (represents APP)
+def main():
+    pass
+
+# Execution
+if __name__ == '__main__':
+    main()
