a5adb9e64913e00d03b62a00ad0f921e707c5d81
FMB_Player <franbalella@hotmail.com.ar>
2025-07-08 21:37:06 -0300

    Added __init__.py to perform a selective wildcard import

---
+++ classes/__init__.py
@@ -0,0 +1,7 @@
+from .Pasajero import Pasajero
+from .Viaje import Viaje
+from .Chofer import Chofer
+from .Ui import Ui  #TODO: Crear la clase Ui
+
+## Aquí se deben insertar todas las clases que requiera main Y SÓLO las que requiera.
+## Clases internas o privadas no deben estar acá.
