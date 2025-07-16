61f930938c5ea93fc1a7fe329ddbd54ed4cf6e77
FMB_Player <franbalella@hotmail.com.ar>
2025-07-16 03:03:35 -0300

    Updated Pasajero - Pasajero now has a unique ID - Some pointers have been made more verbose for easy interpretation and IDE assistance. - Added verbose code

--- classes/Pasajero.py
+++ classes/Pasajero.py
@@ -2,30 +2,41 @@ from classes.Direccion import Direccion
 from EnumMetodoPago import MetodoPago
 
 class Pasajero:
+
+    _id = 1
     def __init__(self, nombre, metodoDePago: MetodoPago=MetodoPago.EFECTIVO):
+        self.__ID = str(Pasajero._id).zfill(5)
+        Pasajero._id += 1
         self.__nombre=nombre
-        self.__direcciones=[]
+        self.__direcciones:list[Direccion]=[]
         self.__metodoDePago=metodoDePago
 
+        print(f"Pasajero creado: {self.__nombre} ({self.__ID}).")
+    
     @property
-    def nombre(self):
+    def ID(self)->str:
+        return self.__ID
+
+    @property
+    def nombre(self)->str:
         return self.__nombre
 
     @property
-    def metodoDePago(self):
+    def metodoDePago(self)->MetodoPago:
         return self.__metodoDePago
 
     @metodoDePago.setter
-    def nombre(self, nombreNuevo):
+    def nombre(self, nombreNuevo:str)->None:
         self.__nombre=nombreNuevo
 
     @property
-    def direcciones(self):
+    def direcciones(self)->list[Direccion]:
         return self.__direcciones
 
-    @direcciones.setter
-    def agregarDireccion(self, direccionNueva: Direccion):
+    def agregarDireccion(self, direccionNueva: Direccion)->None:
         if isinstance(direccionNueva, Direccion):
             self.__direcciones.append(direccionNueva)
+
+            print(f"Dirección agregada: {direccionNueva.calle} {direccionNueva.altura} a pasajero {self.__ID}.")
         else:
             raise ValueError("Dirección no válida")
