7347ac2cfc0987210aba9fb5860de5aa872b8329
FMB_Player <franbalella@hotmail.com.ar>
2025-07-16 02:55:35 -0300

    Updated Chofer - Removed method overload, it was knowingly useless, but it's still commented. - Reformated available setter and aceptarViaje

--- classes/Chofer.py
+++ classes/Chofer.py
@@ -1,4 +1,3 @@
-from typing import overload     # Decorator
 from classes.Historial import Historial
 from classes.Auto import Auto
 from classes.ZonaDeTrabajo import ZonaDeTrabajo
@@ -7,16 +6,19 @@ from classes.Viaje import Viaje
 class Chofer():
 
     _id = 1
-    def __init__(self, auto: Auto, nombre: str, zonasDeTrabajo: list[ZonaDeTrabajo]):
+
+    def __init__(self, Auto: Auto, nombre: str, zonasDeTrabajo: list[ZonaDeTrabajo]):
         self.__ID = str(Chofer._id).zfill(5)
         Chofer._id += 1
-        self.__auto = auto
+        self.__auto = Auto
         self.__nombre = nombre
         self.__zonasDeTrabajo = zonasDeTrabajo
         self.__available = False
         self.__historial = Historial()
 
-    @overload
+        print(f"Chofer creado: {self.__nombre} ({self.__ID}).")
+
+    """ @overload
     def __init__(self, marcaAuto:str, modeloAuto:str, nombre: str, zonasDeTrabajo: list[ZonaDeTrabajo]):
         self.__ID = str(Chofer._id).zfill(5)
         Chofer._id += 1
@@ -24,7 +26,7 @@ class Chofer():
         self.__nombre = nombre
         self.__zonasDeTrabajo = zonasDeTrabajo
         self.__available = False
-        self.__historial = Historial()
+        self.__historial = Historial() """  #Unused. Sobrecarga para crear el auto por parámetros. Se reemplaza por solo invocar el constructor de Autos perse para evitar código dependiente.
 
     @property
     def ID(self):
@@ -60,14 +62,22 @@ class Chofer():
         return self.__available
     
     @available.setter
-    def available(self):
-        self.__available = not self.__available
+    def available(self, value:bool):
+        self.__available = value
+
+        if self.__available:
+            print(f"Chofer {self.__ID} ahora disponible.")
+        else:
+            print(f"Chofer {self.__ID} no disponible.")
 
     def aceptarViaje(self, viaje: Viaje):
         if not self.__available:
             raise ValueError("El chofer no esta disponible")
         self.__historial.agregarViaje(viaje)
         self.__available = False
+        viaje.asignarChofer(self)
+
+        print(f"Chofer {self.__ID} aceptó un nuevo viaje.")
 
     def enviarCodigoViaje(self, code: str):     #FIXME
         "This is trashy code. Lo único que hace es devolver el code que se debería recibir desde una UI y devuelve el dato como si se tratara de un paso de fetch."
