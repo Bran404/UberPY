53ed5dce6f6d671487e8a35116d4b11efc28c36a
FMB_Player <franbalella@hotmail.com.ar>
2025-07-16 04:40:36 -0300

    Updated main. - main almost fully remade. - main is now very verbose, both in main itself and in some classes import's methods. - main's process is now more linear and verbose. - Values used along the whole program are now explicitly typed (Some have missing types from import loop issues.

--- main.py
+++ main.py
@@ -1,133 +1,125 @@
-from .classes import *   # Import required-Only classes
-import time
+from classes import *   # Import required-Only classes
+from time import sleep
 # Resources initialization
-pass
+print("Inicialización del programa...")
+input("Presione enter para continuar...")
+sleep(2)
+
+#Creacion de chofer
+chofer = Chofer(Auto("Toyota", "Corolla"), "Carlos", [ZonaDeTrabajo("Ituzaingó")])
+chofer.available = True
+print(f"Chofer creado: {chofer.nombre} con auto {chofer.auto.marca} {chofer.auto.modelo} y zona de trabajo {chofer.zonasDeTrabajo[0].codigo}")
+    
+# Creación de pasajeros
+pasajeros: list[Pasajero] = []
+    
+# Carga de pasajero principal
+pasajeroPrincipal = Pasajero("Ana")
+pasajeros.append(pasajeroPrincipal)
+pasajeroPrincipal.agregarDireccion(Direccion("Avenida Siempreviva",742))
+
+print("iniciando main...")
+input("Presione enter para continuar...")
+sleep(3)
+
 
 # Main (represents APP)
 def main():
-    #Creacion de chofer
-    chofer = Chofer(Auto("Toyota", "Corolla"), "Carlos", [ZonaDeTrabajo("Ituzaingó")])
-    print(f"Creacion de clases")
-    print(f"Chofer creado: {chofer.nombre} con auto {chofer.auto.marca} {chofer.auto.modelo}")
-    
-    print("\nBienvenido a UberPY") # Ejemplo solicitar un viaje
-    # Creación de pasajeros
-    pasajeros = []
-    
-    # Carga de pasajero principal
-    pasajeroPrincipal = Pasajero("Ana")
-    pasajeros.append(pasajeroPrincipal)
-
     print("Solicitando un viaje...")
+    sleep(1)
+
     print("Formulario de viaje")
-    #Selecciona un destino
-    print("\nSeleccion de destino:")
-    # Ejemplo de ubicacion actual GPS
-    origen = "Los pozos"
-    print(f"Ubicacion actual: {origen}")
-    destino = input("Ingrese el destino del viaje: ")
-    direccion = Direccion(origen, destino)
-    pasajeroPrincipal.agregarDireccion(direccion)
-    
-    # Selecciona el tipo de viaje
-    print("\nSeleccion de tipo de viaje:")
-    print("Listado de tipos de viaje:")
-    listadoTiposViaje = [tipo.value for tipo in EnumTipoViaje.TipoViaje] #Carga de tipos de viaje
-    for tipo in listadoTiposViaje:
-        print(f"- {tipo}")
+    sleep(1)
 
-    while True:
-        try:
-            tipoDeViaje = input("Ingrese el tipo de viaje: ").capitalize()
-            tipoViaje = TipoViaje(tipoDeViaje)
-            if tipoViaje.getTipoViaje() == EnumTipoViaje.TipoViaje.Compartido.value:
-                print("Tipo de viaje compartido seleccionado.")
-                #Ingresa los otros pasajeros
-                while True:
-                    nombrePasajero = input("Ingrese el nombre del pasajero (o 'salir' para terminar): ")
-                    if nombrePasajero.lower() == 'salir':
-                        break
-                    nuevoPasajero = Pasajero(nombrePasajero)
-                    pasajeros.append(nuevoPasajero)
+    print("rellenado del formulario...")
+    sleep(5)
 
-            break
-        except ValueError as e:
-            print(f"Error: {e}")
+    #Se obtiene la ubicación actual del usuario
+    inicio=pasajeros[0].direcciones[0]
+    print(f"Ubicación actual: {inicio.calle} {inicio.altura}")
+    sleep(1)
 
-    print("Informacion de Solicitud de viaje:")#Valida datos del viaje(sin instancia de viaje aun)
-    print(f"Pasajero: {pasajeroPrincipal.getNombre()}")
-    print(f"Pasajeros adicionales: {len(pasajeros) - 1}")
-    print(f"Ruta de viaje: \nOrigen: {direccion.getOrigen()}\nDestino: {direccion.getDestino()}")
-    print(f"Tipo de viaje seleccionado: {tipoViaje.getTipoViaje()}")
+    calle="Autopista Nuncamuerta"
+    altura=368
+    fin=Direccion(calle,altura)
+    print(f"Solicitado destino: {calle} {altura}")
+    sleep(2)
 
-    # Selecciona el metodo de pago
-    print("\nSeleccion de metodo de pago:")
-    print("Listado de métodos de pago:")
-    listadoMetodosPago = [metodo.value for metodo in EnumMetodoPago.MetodoPago]  # Carga de métodos de pago
-    for metodo in listadoMetodosPago:
-        print(f"- {metodo}")
-    while True:
-        try:
-            metodoPago = input("Ingrese el método de pago: ").capitalize()
-            metodoDePago = MetodoPago(metodoPago)
-            break
-        except ValueError as e:
-            print(f"Error: {e}")
+    tipoViaje=TipoViaje.INDIVIDUAL
+    print(f"Seleccionado un viaje {tipoViaje.value}")
+    sleep(1)
 
+    print("Procesando solicitud de viaje...")
+    sleep(0.5)
+    print("Información de Solicitud de viaje:") # Valida datos del viaje(sin instancia de viaje aun)
+    sleep(0.5)
+    print(f"Pasajero: {pasajeros[0].nombre}")
+    sleep(0.5)
+    print(f"Ruta de viaje:[",f"  Origen: {inicio.calle} {inicio.altura}",f"  Destino: {fin.calle} {fin.altura}","]", sep="\n")
+    sleep(0.5)
+    print(f"Tipo de viaje seleccionado: {tipoViaje.value}")
+    sleep(0.5)
+    print(f"Método de pago (Obtenido de la configuración del usuario): {pasajeros[0].metodoDePago.value}")
+    input("Presione enter para continuar...")
     print("\n")
-    # Crea el pago - Procesa Pago
-    pago = Pago(metodoDePago, tipoViaje)
+    sleep(0.5)
+
 
     # Crea el viaje - Pasajero confirma el viaje
-    viaje = Viaje(pasajeros, pago)
+    viaje = Viaje(pasajeros,inicio,fin,tipoViaje)
     print("Buscando choferes disponibles...")
-
-    time.sleep(3)  # Simula la búsqueda de choferes
+    sleep(5)  # Simula la búsqueda de choferes
 
     # Chofer acepta el viaje
     chofer.aceptarViaje(viaje)
     print(f"Chofer {chofer.nombre} ha aceptado el viaje.")
+    sleep(1)
 
-    # APP asigna chofer al viaje
-    viaje.asignarChofer(chofer)
-    
-    viaje.generarCodigoViaje()
     print(f"Código de viaje generado: {viaje.codigoViaje}") # Vista del pasajero
+    sleep(1)
 
     # Seguimiento del viaje
     viaje.seguimientoViaje()
 
-    time.sleep(6) # Simula el tiempo de chofer a pasajero
+    sleep(6) # Simula el tiempo de chofer a pasajero
 
     print("\nEl chofer ha llegado al punto de encuentro.")
-    codigo = input("Ingrese el código de viaje para empezar el viaje: ") # Vista del chofer 
-    if codigo == viaje.codigoViaje:
-        print("Código de viaje confirmado. El viaje ha comenzado.")
-        time.sleep(4) # Simula el tiempo del viaje
-        # Calcular el total del viaje
-        total = viaje.calcularViaje()
-        
-        print("\n")
-        print("Viaje finalizado")
-        print(f"Total del viaje: ${total}")
-        viaje.setEstadoViaje(EstadoPago.PAGADO.value)  # Cambia el estado del viaje a PAGADO
-        print(f"Estado del viaje: {viaje.estadoViaje}") # Vista del pasajero
-        opcionCalificar = input("Desea calificar el viaje? (s/n): ").lower() # Vista del pasajero
-        if opcionCalificar == 's':
-            # Calificación del viaje
-            while True:
-                try:
-                    calificacion = input("Ingrese la calificación del viaje (0 a 5): ")
-                    viaje.setCalificacionViaje(int(calificacion))
-                    print(f"Calificación del viaje: {viaje.calificacion}")
-                    print("Gracias por calificar el viaje.")
-                    break
-                except ValueError as e:
-                    print(f"Error: {e}")
+    codigo = ""
+    while codigo != viaje.codigoViaje:
+        codigo = input("Ingrese el código de viaje para empezar el viaje: ") # Vista del chofer 
+        if codigo == viaje.codigoViaje:
+            print("Código de viaje confirmado. El viaje ha comenzado.")
+            total = viaje.pago.total
+            print("\n")
+            sleep(8) # Simula el tiempo del viaje
+            # Calcular el total del viaje
+            
+            viaje.estadoViaje = EstadoViaje.FINALIZADO
+            print("Viaje finalizado")
+            sleep(1)
+
+            print(f"Total del viaje: ${total}") #Este dato también se guarda en viaje.pago.subtotal, esto es por demo
+            sleep(1)
+
+            print(f"Estado del viaje: {viaje.estadoViaje.value}") # Vista del pasajero
+            sleep(2)
+
+            opcionCalificar = input("Desea calificar el viaje? (s/n): ").lower() # Vista del pasajero
+            if opcionCalificar == 's':
+                # Calificación del viaje
+                while True:
+                    try:
+                        calificacion = input("Ingrese la calificación del viaje (0 a 5): ")
+                        viaje.calificacion = int(calificacion)
+                        print(f"Calificación del viaje: {viaje.calificacion}")
+                        print("Gracias por calificar el viaje.")
+                        break
+                    except ValueError as e:
+                        print(f"Error: {e}")
+            else:
+                print("Gracias por usar UberPY. ¡Hasta luego!")
         else:
-            print("Gracias por usar UberPY. ¡Hasta luego!")
-    else:
-        print("Código de viaje incorrecto. El viaje no puede comenzar.")
+            print("Código de viaje incorrecto. El viaje no puede comenzar.")
 
 # Execution
 if __name__ == '__main__':
