32e3f9dc5e218f5de605e3cd2904ee17f0f9c52d
FMB_Player <franbalella@hotmail.com.ar>
2025-07-16 03:01:01 -0300

    Updated Pago - Removed now unused import. - Updated subtotal attribute to be by parameter and not an always-forced. - Reformated methods and added verbose code

--- classes/Pago.py
+++ classes/Pago.py
@@ -1,16 +1,15 @@
 from classes.EnumEstadoPago import EstadoPago
 from classes.EnumTipoViaje import TipoViaje
 from classes.EnumMetodoPago import MetodoPago
-from classes.Pasajero import Pasajero
 
 class Pago:
-    def __init__(self, metodoDePago: MetodoPago, tipoViaje: TipoViaje):
+    def __init__(self, metodoDePago: MetodoPago, tipoViaje: TipoViaje, subtotal: float = 5000):
         if metodoDePago not in MetodoPago:
             raise ValueError(f"Metodo de pago no valido: {metodoDePago}")
         self.__metodoDePago = metodoDePago
         self.__estadoPago = EstadoPago.PENDIENTE
         self.__tipoViaje = tipoViaje
-        self.__subtotal = self.subtotal = 5000 # Valor de ejemplo, logicamente el valor deberia de ser calculando la distancia del viaje
+        self.__subtotal = subtotal # Valor de ejemplo, logicamente el valor deberia de ser calculando la distancia del viaje
 
     @property
     def metodoDePago(self):
@@ -19,23 +18,34 @@ class Pago:
     @property
     def estadoPago(self):
         return self.__estadoPago
+
+    @estadoPago.setter
+    def estadoPago(self, estadoPago: EstadoPago)->None:
+        if estadoPago in EstadoPago:
+            self.__estadoPago = estadoPago
+
+            print(f"Estado de pago actualizado: {self.__estadoPago}")
     
     @property
     def tipoViaje(self):
         return self.__tipoViaje
     
-    def calcularTotal(self, pasajeros: list[Pasajero]):
+    def calcularTotal(self, cant_pasajeros:int)->int|float:
+
+        print(f"Calculando total...")
+        print(f"Precio base: {self.__subtotal}")
         match self.__tipoViaje:
             case TipoViaje.Individual:
-                total = self.__subtotal
+                return self.__subtotal
             case TipoViaje.Compartido:
-                total = self.__subtotal / len(pasajeros)
+                self.__subtotal = self.__subtotal / cant_pasajeros
             case TipoViaje.Programado:
-                total = self.__subtotal * 0.8
+                self.__subtotal = self.__subtotal * 0.8
             case TipoViaje.Comfort:
-                total = self.__subtotal * 2.0
+                self.__subtotal = self.__subtotal * 2.0
             case TipoViaje.Rapido:
-                total = self.__subtotal * 1.2
-            case _:
-                total = self.__subtotal     # Error case. Se calcula individual
-        return total
+                self.__subtotal = self.__subtotal * 1.2
+        
+        print(f"Total calculado: {self.__subtotal}")
+        
+        return self.__subtotal
